34e83c5d256a800497968b21e54d9713abcd2a83
dina hellal <dinahellal12@gmail.com>
2025-02-19 13:16:25 +0200

    Edit: Hero Section Styles

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,20 +1,17 @@
 function Hero() {
   return (
-    <section class="hero">
-    <div class="container">
-      <div class="user">
-        <h1>
-          Hello <br />
-          I’m Ridho
-        </h1>
-        <p>Freelancer Web Developer</p>
-        <input type="submit" value="Hire Me" />
+    <section class="hero bg-gray-300">
+      <div class="container flex justify-center gap-20">
+        <div class="user">
+          <h1 className="text-7xl font-bold text-gray-950 pt-40 ">Hello <br />I’m Ridho</h1>
+          <p className=" text-l text-gray-900 mt-2">Freelancer Web Developer</p>
+          <input type="submit" value="Hire Me" className="bg-gray-950 text-white pl-4 pr-4 pt-1 pb-1 mt-3 rounded-md" />
+        </div>
+        <div class="user-image">
+          <img src="/user.webp" alt="user" />
+        </div>
       </div>
-      <div class="user-image">
-        <img src="/user.webp" alt="user" />
-      </div>
-    </div>
-  </section>
+    </section>
   );
 }
-  export default Hero;
+export default Hero;
