068e8772958f147d8a636f41bc155e14bc6d21f7
dina hellal <dinahellal12@gmail.com>
2025-02-19 13:02:04 +0200

    Add: Tailwind & Edit: Navbar CSS

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,19 @@
 
 function Navbar() {
   return (
-    <header>
-      <div className="navbar">
-        <div className="container">
-          <div className="logo">
+    <header className="relative p-15">
+      <div className="navbar bg-gray-950 ">
+        <div className="container p-10 m-auto flex justify-between items-center flex-wrap text-white">
+          <div className="logo text-3xl">
             <a href="/">R</a>
           </div>
           <div>
-            <label htmlFor="toggle" className="toggle">
+            <label htmlFor="toggle" className="toggle lg:hidden md:block absolute right-10 top-12">
               <img src="/menu.webp" alt="toggle icon" />
             </label>
-            <input type="checkbox" id="toggle" />
+            <input type="checkbox" id="toggle" className="hidden" />
 
-            <ul>
+            <ul className="  lg:flex gap-20 ">
               <li><a href="#Home">Home</a></li>
               <li><a href="#About">About</a></li>
               <li><a href="#Services">Services</a></li>
