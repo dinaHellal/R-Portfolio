834d7cbc3781a0acd52b32f109723f2c5288f655
dina hellal <dinahellal12@gmail.com>
2025-02-20 21:56:04 +0200

    Update: Blog Section Style

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -2,40 +2,41 @@
 function Blog() {
   return (
 
-<section class="blog">
+<section class="blog p-20 ">
 <div class="container">
-  <h2>Blog</h2>
-  <div class="blogs-list">
-    <div class="blog-item">
-      <div class="blog-image">
+  <h2 className="text-5xl font-bold text-gray-950 mb-6 text-center">Blog</h2>
+  <div class="blogs-list flex flex-wrap gap-10 ml-40  ">
+    <div class="blog-item  flex bg-white border rounded-md w-96  ">
+      <div class="blog-image  w-96 h-52 rounded-md bg-gray-600">
       </div>
-      <div class="blog-info">
-        <h3>Learn HTMl</h3>
-        <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
-      </div>
-    </div>
-    <div class="blog-item">
-      <div class="blog-image">
-      </div>
-      <div class="blog-info">
-        <h3>Learn HTMl</h3>
-        <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
+      <div class="blog-info text-center bg-gray-300 p-10">
+        <h3 className="text-xl text-gray-950">Learn HTMl</h3>
+        <p className="text-gray-800">Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
       </div>
     </div>
-    <div class="blog-item">
-      <div class="blog-image">
+      
+    <div class="blog-item  flex bg-white border rounded-md w-96  ">
+    <div class="blog-image  w-96 h-52 rounded-md bg-gray-600">
       </div>
-      <div class="blog-info">
-        <h3>Learn HTMl</h3>
-        <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
+      <div class="blog-info text-center bg-gray-300 p-10">
+        <h3 className="text-xl text-gray-950">Learn HTMl</h3>
+        <p className="text-gray-800">Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
       </div>
     </div>
-    <div class="blog-item">
-      <div class="blog-image">
+    <div class="blog-item  flex bg-white border rounded-md w-96  ">
+      <div class="blog-image  w-96 h-52 rounded-md bg-gray-600">
       </div>
-      <div class="blog-info">
-        <h3>Learn HTMl</h3>
-        <p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
+      <div class="blog-info text-center bg-gray-300 p-10">
+        <h3 className="text-xl text-gray-950">Learn HTMl</h3>
+        <p className="text-gray-800">Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
+      </div>
+    </div>
+    <div class="blog-item  flex bg-white border rounded-md w-96  ">
+      <div class="blog-image  w-96 h-52 rounded-md bg-gray-600">
+      </div>
+      <div class="blog-info text-center bg-gray-300 p-10">
+        <h3 className="text-xl text-gray-950">Learn HTMl</h3>
+        <p className="text-gray-800">Lorem ipsum dolor sit amet, consecte adipiscing elit. Proin tristique elit nec. lorem ipsum</p>
       </div>
     </div>
   </div>
